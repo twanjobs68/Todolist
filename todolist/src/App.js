af3c5f1ecabf981bb6ae02fa6550cd382b9425f3
Antwan Grant <twanjobs68@gmail.com>
2021-05-02 20:50:03 -0400

    initial commit

---
+++ todolist/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  /*State is an object used by react to determine when to render and how it will behave in the vittural dom
+  const
+
+
+
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/Components/Todoform.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Todoform(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Todoform;
